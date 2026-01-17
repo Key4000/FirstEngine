e524ca030616485f70f331739adc8b4aabd2b0e7
Павел <107314499+Key4000@users.noreply.github.com>
2026-01-17 10:52:11 +0500

    Add Camera class with projection modes and methods

---
+++ FirstEngineCore/includes/FirstEngineCore/Camera.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+#include <glm/ext/matrix_float4x4.hpp>
+
+namespace FirstEngine {
+
+    class Camera
+    {
+    public:
+        //способ проекции 
+        enum class ProjectionMode
+        {
+            Perspective,
+            Orthographic
+        };
+        //конструктор(позицию камеры, и направление, способ проекции)
+        Camera(const glm::vec3& position = { 0, 0, 0 },
+               const glm::vec3& rotation = { 0, 0, 0 },
+               const ProjectionMode projection_mode = ProjectionMode::Perspective);
+        //позиция, поворот 
+        void set_position(const glm::vec3& position);
+        void set_rotation(const glm::vec3& rotation);
+        void set_position_rotation(const glm::vec3& position, const glm::vec3& rotation);
+        //установка способа проекции
+        void set_projection_mode(const ProjectionMode projection_mode);
+        //взять матрицу камеры 
+        glm::mat4 get_view_matrix() const { return m_view_matrix; }
+        //взять проекцию 
+        glm::mat4 get_projection_matrix() const { return m_projection_matrix; }
+
+    private:
+        //обновить 
+        void update_view_matrix();
+        //обновить 
+        void update_projection_matrix();
+        //позиция, поворот 
+        glm::vec3 m_position;
+        glm::vec3 m_rotation;
+        //способ проекции 
+        ProjectionMode m_projection_mode
+        //матрица перехода к камере 
+        glm::mat4 m_view_matrix;
+        //матрица добавления проекции 
+        glm::mat4 m_projection_matrix;
+    };
+}
