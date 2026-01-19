922553a26e6a56fc6ad1bafe38d96cd50a7c7612
Павел <107314499+Key4000@users.noreply.github.com>
2026-01-20 00:16:56 +0500

    Update main.cpp

--- FirstEngineEditor/src/main.cpp
+++ FirstEngineEditor/src/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <memory>
 #include <FirstEngineCore/Application.hpp>
+#include <imgui/imgui.h>
 
 class FirstEngineEditor : public FirstEngine::Application
 {
 	virtual void on_update()  override
 	{
 		//std::cout << "Update frame: " << frame++ << std::endl;
+		virtual void on_ui_draw() override
+    {
+        ImGui::Begin("Editor");
+        ImGui::SliderFloat3("camera position", camera_position, -10.f, 10.f);
+        ImGui::SliderFloat3("camera rotation", camera_rotation, 0, 360.f);
+        ImGui::Checkbox("Perspective camera", &perspective_camera);
+        ImGui::End();
+    }
+
+
 	}
 
 	int frame = 0;
@@ -18,7 +29,7 @@ int main()
 
 	int returnCode = pFirstEngineEditor->start(1024, 768, "FirstEngine Editor");
 
-	std::cin.get();
+	//std::cin.get();
 
 	return returnCode;
 }
