3c324c597e2524e3d895d75ce4902574038efcab
Павел <107314499+Key4000@users.noreply.github.com>
2026-01-19 21:54:45 +0500

    Update Application.cpp

--- FirstEngineCore/src/FirstEngineCore/Application.cpp
+++ FirstEngineCore/src/FirstEngineCore/Application.cpp
@@ -7,6 +7,65 @@
 
 namespace FirstEngine {
 
+	//vertex точек и цвета 
+	GLfloat positions_colors2[] =
+	{
+	   -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 1.0f,
+	   -0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 1.0f,
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f
+	};
+
+	//индексы для index buffer
+	GLuint indices[] = {
+		0, 1, 2, 3, 2, 1
+	};
+
+	const char* vertex_shader =
+		R"(#version 460
+           layout(location = 0) in vec3 vertex_position;
+           layout(location = 1) in vec3 vertex_color;
+           uniform mat4 model_matrix;  
+           uniform mat4 view_projection_matrix; 
+           out vec3 color;
+           void main() {
+              color = vertex_color;
+              gl_Position = view_projection_matrix *  model_matrix * vec4(vertex_position, 1.0);
+           }
+        )";
+
+
+	const char* fragment_shader =
+		R"(#version 460
+           in vec3 color;
+           out vec4 frag_color;
+           void main() {
+              frag_color = vec4(color, 1.0);
+           }
+        )";
+
+	//указатель на шейдерную программу
+	std::unique_ptr<ShaderProgram> p_shader_program;
+
+
+	//указатели vertex буфер 
+	std::unique_ptr<VertexBuffer> p_positions_colors_vbo;
+	//index buffer
+	std::unique_ptr<IndexBuffer> p_index_buffer;
+	//vertex array
+	std::unique_ptr<VertexArray> p_vao;
+
+	float scale[3] = { 1.f, 1.f, 1.f };
+	float rotate = 0.f;
+	float translate[3] = { 0.f, 0.f, 0.f };
+	//камера 
+	float camera_position[3] = { 0.f, 0.f, 1.f };
+	float camera_rotation[3] = { 0.f, 0.f, 0.f };
+	bool perspective_camera = false;
+	Camera camera;
+
+//
+
 	Application::Application() {
 		LOG_INFO("Starting Application ");
 	}
