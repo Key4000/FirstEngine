f748d10a8c9505352f033e5208a2667998dcaeb7
Павел <107314499+Key4000@users.noreply.github.com>
2026-01-20 23:38:28 +0500

    Translate, and rotate camera on key pressed

--- FirstEngineEditor/src/main.cpp
+++ FirstEngineEditor/src/main.cpp
@@ -2,12 +2,57 @@
 #include <memory>
 #include <FirstEngineCore/Application.hpp>
 #include <imgui/imgui.h>
+#include <FirstEngineCore/Input.hpp>
 
 class FirstEngineEditor : public FirstEngine::Application
 {
+    //каждый кадр в основном цикле!!! 
 	virtual void on_update()  override
 	{
-		//std::cout << "Update frame: " << frame++ << std::endl;
+		//-----------------движение камеры по клавишам--------------//
+       if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_W))
+        {
+            camera_position[2] -= 0.01f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_S))
+        {
+            camera_position[2] += 0.01f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_A))
+        {
+            camera_position[0] -= 0.01f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_D))
+        {
+            camera_position[0] += 0.01f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_E))
+        {
+            camera_position[1] += 0.01f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_Q))
+        {
+            camera_position[1] -= 0.01f;
+        }
+
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_UP))
+        {
+            camera_rotation[0] += 0.5f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_DOWN))
+        {
+            camera_rotation[0] -= 0.5f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_RIGHT))
+        {
+            camera_rotation[1] -= 0.5f;
+        }
+        if (SimpleEngine::Input::IsKeyPressed(SimpleEngine::KeyCode::KEY_LEFT))
+        {
+            camera_rotation[1] += 0.5f;
+		}
+		//------------------------------------------------------------------//
+		
 		virtual void on_ui_draw() override
     {
         ImGui::Begin("Editor");
