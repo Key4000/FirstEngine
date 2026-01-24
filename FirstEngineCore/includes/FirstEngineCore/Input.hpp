461169efb653158621dfa8a2e75098b2a1062f40
Павел <107314499+Key4000@users.noreply.github.com>
2026-01-24 10:57:56 +0500

    Add mouse button input methods to Input.hpp

--- FirstEngineCore/includes/FirstEngineCore/Input.hpp
+++ FirstEngineCore/includes/FirstEngineCore/Input.hpp
@@ -11,7 +11,10 @@ namespace FirstEngine {
         static void PressKey(const KeyCode key_code);        
         static void ReleaseKey(const KeyCode key_code);
 
-    private:
+        static bool IsMouseButtonPressed(const MouseButton mouse_button);
+        static void PressMouseButton(const MouseButton mouse_button);        
+        static void ReleaseMouseButton(const MouseButton mouse_button);
+private:
         static bool m_keys_pressed[];
         static bool m_mouse_buttons_pressed[];
 };
