159706b0cd64efdb5340dc7ecd23eced281254f4
Павел <107314499+Key4000@users.noreply.github.com>
2026-01-24 11:30:51 +0500

    Add mouse button input handling functions

--- FirstEngineCore/src/FirstEngineCore/Input.cpp
+++ FirstEngineCore/src/FirstEngineCore/Input.cpp
@@ -20,4 +20,20 @@ namespace FirstEngine {
     {
         m_keys_pressed[static_cast<size_t>(key_code)] = false;
     }
+    //если мышка нажата 
+    bool Input::IsMouseButtonPressed(const MouseButton mouse_button)
+    {
+        return m_mouse_buttons_pressed[static_cast<size_t>(mouse_button)];
+    }
+    //нажатие мышки 
+    void Input::PressMouseButton(const MouseButton mouse_button)
+    {
+        m_mouse_buttons_pressed[static_cast<size_t>(mouse_button)] = true;
+    }
+    //мышку отпустили 
+    void Input::ReleaseMouseButton(const MouseButton mouse_button)
+    {
+        m_mouse_buttons_pressed[static_cast<size_t>(mouse_button)] = false;
+    }
+}
 }
